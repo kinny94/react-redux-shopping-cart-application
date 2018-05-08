5b331e151a35bd054d6960b0a4a2be0f20dfac0a
kinny94 <ad.sam007@gmail.com>
2018-05-08 16:45:32 -0400

    creating actions and reducers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { createStore } from 'redux';
 
-console.log( "Hello" );
 //Step 3 - define the reducers
-const reducer = ( state = 0, action ) => {
+const reducer = ( state = { books:[] }, action ) => {
     switch( action.type ){
-        case "INCREMENT":
-            return state + action.payload;
+        case "POST_BOOK":
+            let books = state.books.concat( action.payload );
+            return { books };
             break;
     }
 
@@ -15,11 +15,37 @@ const reducer = ( state = 0, action ) => {
 // Step 1 - create the store
 const store = createStore( reducer );
 store.subscribe(() => {
-    console.log( 'current state is: '  + store.getState());
+    console.log( 'current state is: ',  store.getState());
 });
 
 // Step 2 - Create and dispatch the actions
 store.dispatch({
-    type: "INCREMENT",
-    payload: 1
+    type: "POST_BOOK",
+    payload: [
+        {
+            id: 1,
+            title: " This is the title of a Book",
+            description: "This is some description about the book!!",
+            price: 40
+        },
+        {
+            id: 2,
+            title: " This is the title of the second Book",
+            description: "This is some description about the second book!!",
+            price: 32
+        }
+    ]
 }); 
+
+// Dispatch a second action - Update Operation
+store.dispatch({
+    type: "POST_BOOK",
+    payload: 
+    [   {
+            id: 3,
+            title: " This is the title of the third Book",
+            description: "This is some description about the third book!!",
+            price: 60
+        }
+    ]
+});
