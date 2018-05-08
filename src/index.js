5e303d318359d8c2b86ab5519b01d79dc9922900
kinny94 <ad.sam007@gmail.com>
2018-05-08 17:06:16 -0400

    Deleting and updating a book using action and reducers

--- src/index.js
+++ src/index.js
@@ -3,9 +3,40 @@ import { createStore } from 'redux';
 //Step 3 - define the reducers
 const reducer = ( state = { books:[] }, action ) => {
     switch( action.type ){
+
         case "POST_BOOK":
             let books = state.books.concat( action.payload );
             return { books };
+            break;
+        
+        case "DELETE_BOOK":
+            const currentBookToDelete = [ ...state.books ];
+            const indexToDelete = currentBookToDelete.findIndex(( book ) => {
+                return book.id === action.payload.id;
+            }); 
+            return { 
+                books: [
+                    ...currentBookToDelete.slice(0, indexToDelete), 
+                    ...currentBookToDelete.slice( indexToDelete + 1 )
+                ]
+            }
+            break;
+
+        case "UPDATE_BOOK": 
+            const currentBookToUpdate = [ ...state.books ];
+            const indexToUpdate = currentBookToUpdate.findIndex(( book ) => {
+                return book.id === action.payload.id
+            });
+            const newBookToUpdate = {
+                ...currentBookToUpdate[ indexToUpdate ],
+                title: action.payload.title
+            }
+            console.log( newBookToUpdate );
+            return {
+                books: [
+                    ...currentBookToUpdate.slice( indexToUpdate + 1 )
+                ]
+            };
             break;
     }
 
@@ -49,3 +80,22 @@ store.dispatch({
         }
     ]
 });
+
+//Delete a book
+store.dispatch({
+    type: "DELETE_BOOK",
+    payload:{
+        id: 3
+    }
+});
+
+//Update a book
+store.dispatch({
+    type: "UPDATE_BOOK",
+    payload: {
+        payload:{
+            id: 2,
+            title: 'This is the new title provided by the update action!' 
+        }
+    }
+})
